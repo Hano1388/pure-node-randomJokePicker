c12d89423e6ac4c841368c1d7c4ab878e8b3581c
Hano <hinsul3113@gmail.com>
2019-07-29 02:53:06 -0700

    created printAJoke function

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ app.config = {
   'timeBetweenJokes': 1000
 };
 
-// TODO: write a function that prints a random joke
+// function that prints a random joke
 app.printAJoke = function() {
+    const allJokes = jokesLib.allJokes();
 
+    const numberOfJokes = allJokes.length;
+
+    const randomNumber = mathLib.getRandomNumber(1,numberOfJokes);
+
+    const selectedJoke = allJokes[randomNumber - 1];
+
+    console.log(selectedJoke);
 }
 
 // TODO: write a function that calls printJoke infinitely
