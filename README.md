bf52e2c1d66a5f55675ceb562637d580f735a308
Hano <hinsul3113@gmail.com>
2019-07-29 02:45:04 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Print Random Jokes to Console
+
+Pure Node.js app that reads through a txt file that has jokes and every minute prints a random joke to console
---
+++ index.js
@@ -0,0 +1,18 @@
+const app = {};
+
+app.config = {
+  'timeBetweenJokes': 1000
+};
+
+// TODO: write a function that prints a random joke
+app.printAJoke = function() {
+
+}
+
+// TODO: write a function that calls printJoke infinitely
+app.infiniteLoop = function() {
+
+}
+
+// and finally invoke the loop
+app.infiniteLoop();
